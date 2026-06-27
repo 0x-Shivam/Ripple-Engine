6303d4052e21053dc6a38922c99405dda4715b32
0x-Shivam <ny753339@gmail.com>
2026-06-27 16:07:30 +0530

    Enhance ButterflyPredictionEngine by adding impact sector and metric nodes to the graph

--- engine.py
+++ engine.py
@@ -15,3 +15,26 @@ class ButterflyPredictionEngine:
         """
 
         #core decision node 
+
+        self.graph.add_node("Proposed_Metro_Hub", type="intervention")
+
+        # Impact sector node 
+
+        self.graph.add_node([
+            ("Mobility_Sector", {"type": "sector"}),
+            ("Environmental_Sector", {"type": "sector"}),
+            ("Economic_Sector", {"type": "sector"})
+
+        
+        ])
+
+
+        # end point meteric nodes 
+
+        self.graph.add_nodes_from([
+            ("Traffic_Flow", {"type": "metric", "base_unit" : "%"}),
+            ("Air_Quality", {"type": "metric", "base_unit" : "%"}),
+            ("Jobs_Created", {"type": "metric", "base_unit" : "%"}),
+            ("Flood_Risk", {"type": "metric", "base_unit" : "%"}),
+            ("Green_Spaces", {"type": "metric", "base_unit" : "%"}),("Property_Value", {"type": "metric", "base_unit" : "%"}),("Noise_Level", {"type": "metric", "base_unit" : "%"})
+        ])
